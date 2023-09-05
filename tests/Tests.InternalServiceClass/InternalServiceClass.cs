f26da55026cf7eced19c46e6b12fab31468bc439
Roy Padina <roypadina@users.noreply.github.com>
2023-09-06 00:08:49 +0300

    Init Version 1.0.0 (#1)

---
+++ tests/Tests.InternalServiceClass/InternalServiceClass.cs
@@ -0,0 +1,12 @@
+﻿#region Using
+
+using Softy.EasyServiceRegistration.Microsoft.DependencyInjection.Core.Attributes;
+
+#endregion
+
+namespace Tests.InternalServiceClass;
+
+public interface IInternalServiceInterface { }
+
+[RegisterService<IInternalServiceInterface>]
+internal class InternalServiceClass : IInternalServiceInterface { }
